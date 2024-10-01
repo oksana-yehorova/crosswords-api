abd3eaab2abd625c744c4a7fccfde772f78c9893
Oksana Yehorova <oe@albedo.dev>
2024-10-01 15:29:49 +0300

    Update crossword_template1.js

--- utils/templates/12/crossword_template1.js
+++ utils/templates/12/crossword_template1.js
@@ -6,59 +6,48 @@ const {
 } = require('../../puzzleGenActionsForPuzzleJinnie');
 
 const generateTemplate = async (puzzleState) => {
-
-  if (!await fillRecordOnGrid(puzzleState, 5, 1, 6, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 4,  0, 10, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 3, 2, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 3,  4, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 6, 5, 6, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 6, 6, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-
-
-  if (!await fillRecordOnGrid(puzzleState, 3,  5, 8, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 3, 9, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 3,  11, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-
+  if (!await fillRecordOnGrid(puzzleState, 4,0, 4, ORIENTATION_ENUM.COLUMN, 'LEFT_DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 4, 0, 0, ORIENTATION_ENUM.ROW, 'DOWN_RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 5, 1, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 3, 2, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6,4, 0, ORIENTATION_ENUM.ROW, 'UP_RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 5, 1, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6, 0, 2, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6, 0, 6, ORIENTATION_ENUM.COLUMN, 'LEFT_DOWN')) return false;
+
   if (!await fillRecordOnGrid(puzzleState, 5, 5, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 7, 4, 3, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 4, 7, 0, ORIENTATION_ENUM.ROW, 'UP_RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 7, 4, 1, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 5, 9, 0, ORIENTATION_ENUM.ROW, 'UP_RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 5, 6, 4, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 5, 11, 0, ORIENTATION_ENUM.ROW, 'UP_RIGHT')) return false;
 
-  if (!await fillRecordOnGrid(puzzleState, 7, 4, 1, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 6, 0, 2, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 4, 7, 2, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 5, 11, 2, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 7, 4, 3, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 4,0, 4, ORIENTATION_ENUM.COLUMN, 'LEFT_DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 3, 2, 4, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 3, 4, 4, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 5, 6, 4, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
-
-  if (!await fillRecordOnGrid(puzzleState, 6, 8, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 6, 10, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 6, 0, 6, ORIENTATION_ENUM.COLUMN, 'LEFT_DOWN')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 5, 3, 6, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 4, 7, 7, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
   if (!await fillRecordOnGrid(puzzleState, 7, 0, 8, ORIENTATION_ENUM.COLUMN, 'LEFT_DOWN')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 6,  0, 9, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 4,  7, 9, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 6,  5, 10, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
+  if (!await fillRecordOnGrid(puzzleState, 5, 1, 6, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6, 0, 11, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 3, 2, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 5, 3, 6, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 4,  0, 10, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 3,  4, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6,  0, 9, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+
+
+  if (!await fillRecordOnGrid(puzzleState, 6, 6, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 4, 7, 2, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 5, 11, 2, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6, 5, 6, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 3,  5, 8, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 5, 9, 2, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6, 8, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 3,  11, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 4,  7, 11, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 4, 7, 7, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6,  5, 10, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 3, 9, 8, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 4,  7, 9, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
+  if (!await fillRecordOnGrid(puzzleState, 6, 10, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
 
   const payload = await finalizePuzzle(puzzleState);
   return payload;
