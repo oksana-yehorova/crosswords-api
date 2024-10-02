c9830c38777d075cc16121121ef11b7bde0a4faa
Oksana Yehorova <oe@albedo.dev>
2024-10-02 17:42:17 +0300

    Update crossword_template2.js

--- utils/templates/12/crossword_template2.js
+++ utils/templates/12/crossword_template2.js
@@ -6,26 +6,43 @@ const {
 } = require('../../puzzleGenActionsForPuzzleJinnie');
 
 const generateTemplate = async (puzzleState) => {
-  if (!await fillRecordOnGrid(puzzleState, 5, 0, 0, ORIENTATION_ENUM.ROW, 'DOWN_RIGHT')) return false;
+  let tmpPuzzleState = JSON.parse(JSON.stringify(puzzleState));
+  let blockGenerated = false;
+  let attempts;
+  const attemptsLimit = 75;
+
+  // top left
+  while (!blockGenerated) {
+    tmpPuzzleState = JSON.parse(JSON.stringify(puzzleState));
+
+    if (!await fillRecordOnGrid(tmpPuzzleState, 4, 0, 1, ORIENTATION_ENUM.COLUMN, 'DOWN')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 4, 0, 3, ORIENTATION_ENUM.COLUMN, 'DOWN')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 4, 2, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 4, 5, 0, ORIENTATION_ENUM.ROW, 'UP_RIGHT')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 6, 0, 2, ORIENTATION_ENUM.COLUMN, 'DOWN')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 5, 0, 0, ORIENTATION_ENUM.ROW, 'DOWN_RIGHT')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 4, 0, 5, ORIENTATION_ENUM.ROW, 'LEFT_DOWN')) continue;
+    if (!await fillRecordOnGrid(tmpPuzzleState, 4, 3, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) continue;
+
+    blockGenerated = await fillRecordOnGrid(puzzleState, 5, 2, 5, ORIENTATION_ENUM.COLUMN, 'DOWN');
+  }
+  puzzleState = JSON.parse(JSON.stringify(tmpPuzzleState))
+  blockGenerated = false;
+  attempts = 0;
+
+
   if (!await fillRecordOnGrid(puzzleState, 5, 6, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 6, 0, 2, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
 
 
   if (!await fillRecordOnGrid(puzzleState, 6, 5, 1, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
 
   if (!await fillRecordOnGrid(puzzleState, 4, 7, 2, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
 
-  if (!await fillRecordOnGrid(puzzleState, 4, 2, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 4, 3, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 4, 5, 0, ORIENTATION_ENUM.ROW, 'UP_RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 4, 7, 0, ORIENTATION_ENUM.ROW, 'DOWN_RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 4, 9, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6, 10, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 4, 11, 0, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
 
-  if (!await fillRecordOnGrid(puzzleState, 4, 1, 1, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
-
-  if (!await fillRecordOnGrid(puzzleState, 4, 0, 3, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6, 5, 3, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
 
   if (!await fillRecordOnGrid(puzzleState, 4, 4, 4, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
@@ -33,9 +50,7 @@ const generateTemplate = async (puzzleState) => {
   if (!await fillRecordOnGrid(puzzleState, 3, 7, 4, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 3, 8, 4, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
 
-  if (!await fillRecordOnGrid(puzzleState, 4, 0, 5, ORIENTATION_ENUM.ROW, 'LEFT_DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6, 1, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
-  if (!await fillRecordOnGrid(puzzleState, 5, 2, 5, ORIENTATION_ENUM.COLUMN, 'DOWN')) return false;
   if (!await fillRecordOnGrid(puzzleState, 5, 8, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6, 9, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
   if (!await fillRecordOnGrid(puzzleState, 6, 11, 5, ORIENTATION_ENUM.ROW, 'RIGHT')) return false;
